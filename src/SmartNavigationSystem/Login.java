376054694a2f21a363df6ed29022f300b81145b9
++ <lsjiajia1209@gmail.com>
2021-10-22 21:05:12 +0800

    update

--- src/SmartNavigationSystem/Login.java
+++ src/SmartNavigationSystem/Login.java
@@ -1,6 +1,5 @@
 package SmartNavigationSystem;
 
-
 import java.io.*;
 import java.util.*;
 
@@ -8,121 +7,116 @@ public class Login {
     private String inpEmail, inpPwd;
     private int tryTimes = 0;
     // private HashMap<String, String> memberList = Register.memberMap;
-    private ControlPanel cp = UserControlPanel.getInstance();
-    private ControlPanel memberCP=MemberControlPanel.getInstance();
+    private ControlPanel cp = new ControlPanel();
     private Register register = new Register();
     
 
-    public void login() throws IOException {
+    public void login() {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Please input email: ");
         this.inpEmail = userInput.nextLine();
         File f=new File("docs\\MemberList");
         Boolean exist=isExist(f, this.inpEmail);
-        if(!exist){
+        if (!exist) {
             System.out.println("Would you like to register one?[Y/N]");
             String ans = userInput.nextLine();
             if (ans.equals("Y")) {
                 register.register();
             }
-        }else{
+        }
+        else {
             System.out.println("Please input password: ");
             this.inpPwd = userInput.nextLine();
-            verifyPwd(userInput);
+            verifyPwd();
         }
         userInput.close();
     }
 
     public boolean isExist(File f, String Email) {
-    try {
-        Scanner scan;
-        scan = new Scanner(f);
-        
-        while(true){
-            if(scan.hasNext()==false) break;
-            if(scan.nextLine().contains(Email)){
-                return true;
-            }
-        } 
-        scan.close();
-    } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    return false;
-    }
-
-    public boolean isPwdCorrect(File f){
+        Scanner scan = null;
         try {
-            Scanner scan;
             scan = new Scanner(f);
-        
-            while(true){
+            while(true) {
                 if(scan.hasNext()==false) break;
-                String[] memberInfo = scan.nextLine().split("\\s+");
-                String email=memberInfo[0];
-                String password=memberInfo[1];
-                if(email.equals(this.inpEmail)&& password.equals(this.inpPwd)){
+                if(scan.nextLine().contains(Email)) {
                     return true;
                 }
-            } 
-            scan.close();
-       } catch (FileNotFoundException e) {
+            }
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+        finally {
+            if (scan != null) {
+                scan.close();
+            }
         }
         return false;
     }
 
-    public void modifyPwd(String newPwd,String inpEmail, File f){
+    public boolean isPwdCorrect(File f){
+        Scanner scan = null;
         try {
-            Scanner scan;
             scan = new Scanner(f);
-        
-            while(true){
+            while (true) {
                 if(scan.hasNext()==false) break;
                 String[] memberInfo = scan.nextLine().split("\\s+");
-                String email=memberInfo[0];
-                String password=memberInfo[1];
+                String email = memberInfo[0];
+                String password = memberInfo[1];
+                if(email.equals(this.inpEmail) && password.equals(this.inpPwd)) {
+                    return true;
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (scan != null) {
+                scan.close();
+            }
+        }
+        return false;
+    }
+
+    public void modifyPwd(String newPwd, String inpEmail, File f){
+        try {
+            Scanner scan = new Scanner(f);
+            while (true) {
+                if(scan.hasNext()==false) break;
+                String[] memberInfo = scan.nextLine().split("\\s+");
+                String email = memberInfo[0];
+                String password = memberInfo[1];
                 if(email.equals(this.inpEmail)){
                     memberInfo[1].replace(password, newPwd);
                 }
             } 
             scan.close();
-       } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void verifyPwd(Scanner userInput) {
-        File f=new File("docs\\MemberList");
-        Boolean pwd=isPwdCorrect(f);
+    public void verifyPwd() {
+        File f = new File("docs\\MemberList");
+        Boolean pwd = isPwdCorrect(f);
         if(pwd){
             System.out.println("Log in successfully");
-            // cp.removeControlPanel(1);
-            // cp.removeControlPanel(2);
-            memberCP.showControlPanel(); 
-            try {
-                memberCP.makeDecision(userInput);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            cp.removeControlPanel(1);
+            cp.removeControlPanel(2);
+            cp.showControlPanel();
+            cp.makeDecision();
         }else{
             // can try three times
             if (tryTimes < 3) {
                 tryTimes += 1;
                 System.out.println("Wrong email/password!");
-                try {
-                    login();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                login();
             } else {
                 resetPwd();
-                memberCP.showControlPanel();
-                try {
-                    memberCP.makeDecision(userInput);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                cp.showControlPanel();
+                cp.makeDecision();
             }
         }
 
@@ -138,6 +132,7 @@ public class Login {
             this.inpPwd = userInput.next();
             File f=new File("docs\\MemberList");
             modifyPwd(this.inpPwd,this.inpEmail,f);
+            // this.memberList.replace(this.inpEmail, this.inpPwd);
             System.out.println("Reset successfully!");
         }
         userInput.close();
