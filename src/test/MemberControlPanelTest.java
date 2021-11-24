e42302d5d4265b43ebc298a6a423971b9c524c8f
lst <71402513+Yecats77@users.noreply.github.com>
2021-11-24 17:24:10 +0800

    Update test cases [11/24/2021]

--- src/test/MemberControlPanelTest.java
+++ src/test/MemberControlPanelTest.java
@@ -7,13 +7,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.alibaba.fastjson.JSONObject;
+
+import SmartNavigationSystem.ExInvalidIndex;
 import SmartNavigationSystem.JsonOperation;
 import SmartNavigationSystem.Login;
 import SmartNavigationSystem.Member;
@@ -24,178 +26,142 @@ public class MemberControlPanelTest {
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
  	private final PrintStream originalOut = System.out;
  	private final InputStream originalIn = System.in;
+ 	
+ 	private final String email = "cs3343g20system@gmail.com";
+ 	
+ 	private Stub_Member m;
 
- 	@Before
- 	public void setUpStreams() {
+	class Stub_Login extends Login {
+ 		public boolean login() {
+ 			return true; 
+ 		}
+ 	}
+		
+ 	class Stub_Member extends Member {
+ 		
+ 		private Stub_Login login;
+ 		
+ 		@Override
+ 		public Stub_Member Login(Scanner scan) {
+ 			this.login = new Stub_Login();
+ 	        login.login();
+ 			return this;
+ 		} 
+ 		
+ 		@Override
+		public void resetPwd(Scanner scan) {
+ 			//pass
+ 		}
+ 		
+ 		@Override
+		public void chooseMode(Scanner scan) {
+ 			//pass
+ 		}
+ 		
+ 		@Override
+		public void CheckInfo() {
+ 			//pass
+ 		}
+ 		
+ 		@Override
+		public void deleteSchedule(Scanner scan) {
+ 			//pass
+ 		}
+ 		
+ 		@Override
+		public void deleteBookmark(Scanner scan) {
+ 			//pass
+ 		}
+ 		
+ 		@Override
+		public void makeSchedule(Scanner scan) {
+ 			//pass
+ 		}
+ 		
+ 		@Override
+ 		public String getEmail() {
+ 			return email;
+ 		}
+ 	}
+ 	
+ 	@Before 	
+ 	public void setUp() throws IOException, ExInvalidIndex {
+		
  	    System.setOut(new PrintStream(outContent));
-	}  
+ 	    
+ 		m = new Stub_Member();
+ 		m.Login(new Scanner(""));
+ 		
+ 		new JsonOperation();
 
+ 		new JsonOperation();
+ 		JSONObject obj = JsonOperation.getMemberInfo(email);
+ 		if (obj == null) {
+ 			JsonOperation.addNewMember(email, "pwd");
+ 		} else {
+ 			JsonOperation.resetPwd(email, "pwd");
+ 			int bookmNum = JsonOperation.getMemberBookmArray(email).size();
+ 			int scheNum = JsonOperation.getMemberScheArray(email).size();
+ 			for (int i = 1; i <= bookmNum; i++) {
+ 				JsonOperation.deleteMemberBookmark(m, 1);
+ 			}
+ 			for (int i = 1; i <= scheNum; i++) {
+ 				JsonOperation.deleteMemberSchedule(m, 1);
+ 			}
+ 		}
+ 		
+ 		MemberControlPanel.getInstance().setMember(m);
+ 		 		
+ 	}
+ 	
  	@Test
  	public void makeDecision_case1() throws IOException {
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("7"));
  		assertEquals(1000, res);
-		
  	}
 	
  	@Test
  	public void makeDecision_case2() throws IOException {
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("\n-7\n7"));
  		assertEquals(1000, res);
-		
  	}
 
  	@Test
  	public void makeDecision_case3() throws IOException {
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("-"));
  		assertEquals(1000, res);
-		
  	}
-
 	
  	@Test
  	public void makeDecision_case4() throws IOException {
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("0"));
  		assertEquals(0, res);
-		
  	}
 	
  	@Test
  	public void makeDecision_case5() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true;
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
- 			@Override
-			public void resetPwd(Scanner in) {
- 				//pass
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("1"));
  		assertEquals(1, res);
-		
  	}
 
  	@Test
  	public void makeDecision_case6() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
-			public void chooseMode(Scanner in) {
- 				//pass
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("2\nCyclingMode"));
  		assertEquals(2, res);
-		
  	}
 
  	@Test
  	public void makeDecision_case7() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
- 			@Override
- 			public void CheckInfo() {
- 				//pass
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("3"));
  		assertEquals(3, res);
-		
  	}
 	
 	
  	@Test
- 	public void makeDecision_case8() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
- 			@SuppressWarnings("unused")
-			public void deleteSchedule(int scheduleIndex) {
- 				//pass
- 			}
- 			@Override
- 			public String getEmail() {
- 				return "cs3343g20system@gmail.com";
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-		
-         PrintWriter pw = new PrintWriter("docs/MemberInfo.json");
-         pw.write("{\"memberInfo\":[{\"bookmarks\":[],\"password\":\"pwd\",\"schedules\":[{\"scheduleIndex\":1,\"scheduleDate\":\"2021/11/6\",\"state\":\"true\",\"event\":\"Cycling Mode: null\"}],\"email\":\"cs3343g20system@gmail.com\"}]}");
-         pw.close();
-
- 		new JsonOperation();
-		
+ 	public void makeDecision_case8() throws IOException, ExInvalidIndex {
+				  
+ 		JsonOperation.addNewSchedule(m, "date", "route");
+ 		
  		String input = "4\n1";
  		System.setIn(new ByteArrayInputStream(input.getBytes()));
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("4\n1"));
@@ -204,253 +170,46 @@ public class MemberControlPanelTest {
  	}
 	
  	@Test
- 	public void makeDecision_case9() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
- 			@SuppressWarnings("unused")
-			public void deleteSchedule(int scheduleIndex) {
- 				//pass
- 			}
- 			@Override
- 			public String getEmail() {
- 				return "cs3343g20system@gmail.com";
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-
-         PrintWriter pw = new PrintWriter("docs/MemberInfo.json");
-         pw.write("{\"memberInfo\":[{\"bookmarks\":[],\"password\":\"pwd\",\"schedules\":[],\"email\":\"cs3343g20system@gmail.com\"}]}");
-         pw.close();
-
- 		new JsonOperation();
-		
+ 	public void makeDecision_case9() throws IOException, ExInvalidIndex {
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("4\n1"));
  		assertEquals(4, res);
-		
  	}
-
 	
  	@Test
  	public void makeDecision_case10() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
- 			@SuppressWarnings("unused")
-			public void deleteBookmark(int bookmarkIndex) {
- 				//pass
- 			}
- 			@Override
- 			public String getEmail() {
- 				return "cs3343g20system@gmail.com";
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-		
-         PrintWriter pw = new PrintWriter("docs/MemberInfo.json");
-         pw.write("{\"memberInfo\":[{\"bookmarks\":[],\"password\":\"pwd\",\"schedules\":[],\"email\":\"cs3343g20system@gmail.com\"}]}");
-         pw.close();
-
- 		new JsonOperation();
-
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("5\n1"));
  		assertEquals(5, res);
-		
  	}
 
  	@Test
  	public void makeDecision_case11() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
-			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
-				return this;
- 			} 
- 			@SuppressWarnings("unused")
-			public void deleteBookmark(int bookmarkIndex) {
- 				//pass
- 			}
- 			@Override
- 			public String getEmail() {
- 				return "cs3343g20system@gmail.com";
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-		
-         PrintWriter pw = new PrintWriter("docs/MemberInfo.json");
-         pw.write("{\"memberInfo\":[{\"bookmarks\":[{\"bookmarkIndex\":1,\"bookmarkType\":\"Cycling Mode: Sha Tin Che Kung Temple -> Hong Kong Heritage Museum\\n\"}],\"password\":\"pwd\",\"schedules\":[],\"email\":\"cs3343g20system@gmail.com\"}]}");
-         pw.close();
-
- 		new JsonOperation();
-
+ 		JsonOperation.addNewBookmark("route", m);
 		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("5\n1"));
  		assertEquals(5, res);
-		
  	}
 
  	@Test
  	public void makeDecision_case12() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
-			public void makeSchedule(Scanner in) {
- 				//pass
- 			}
-			public void chooseMode(Scanner in) {
- 				//pass
- 			}
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
- 		new JsonOperation();
-
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("6\n2021/11/03\n1"));
  		assertEquals(6, res);
-		
  	}
 
  	@Test
  	public void makeDecision_case13() throws IOException {
-		
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
-			public void makeSchedule(Scanner in) {
- 				//pass
- 			}
- 		}
-	
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("6\n2021/11/03\n2"));
  		assertEquals(6, res);
-		
  	}
  	@Test
  	public void makeDecision_case14() throws IOException {
-	
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			}
-			public void makeSchedule(Scanner in) {
- 				//pass
- 			}
- 		}
-	
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
- 
- 		MemberControlPanel.getInstance().setMember(m);
-		
  		int res = MemberControlPanel.getInstance().makeDecision(new Scanner("6\n2021/11/03\n3"));
  		assertEquals(6, res);
-		
  	}
 
  	@Test
  	public void getMember_case1 () {
- 		class Stub_Login extends Login {
- 			public boolean login() {
- 				return true; 
- 			}
- 		}
-		
- 		class Stub_Member extends Member {
- 			private Stub_Login login;
- 			public Stub_Member Login() {
- 				this.login = new Stub_Login();
- 		        login.login();
- 				return this;
- 			} 
- 		}
-		
- 		Stub_Member m = new Stub_Member();
- 		m.Login();
-		  
- 		MemberControlPanel.getInstance().setMember(m);
  		Member res = MemberControlPanel.getInstance().getMember();
  		assertEquals(m, res);
  	}
-
 
  	@After
  	public void restoreStreams() {
